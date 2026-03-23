b5104dc03d18ff3bc29e107757139305a090f4cb
cooen <42770826+cooen@users.noreply.github.com>
2026-03-23 10:17:54 +0800

    light.py
    尝试解决灯带色温上报引起的报错

--- custom_components/terncy/light.py
+++ custom_components/terncy/light.py
@@ -1,125 +1,193 @@
-"""Light platform support for Terncy."""
-
+"""Terncy light platform."""
 import logging
-import math
-
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS,
-    ATTR_COLOR_TEMP_KELVIN,
+    ATTR_COLOR_TEMP,
     ATTR_HS_COLOR,
     ColorMode,
     LightEntity,
-    LightEntityFeature,
+    ColorMode,
 )
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.util import color as color_util
 
-from .hass.entity import TerncyEntity
-from .hass.entity_descriptions import TerncyLightDescription
-from .types import AttrValue
-from .utils import get_attr_value
+# 导入必要的常量
+SUPPORT_BRIGHTNESS = 1
+SUPPORT_COLOR_TEMP = 2
+SUPPORT_COLOR = 4
+
+from homeassistant.const import CONF_DEVICE_ID
+from .const import DOMAIN, TERNCY_HUB_ID
 
 _LOGGER = logging.getLogger(__name__)
 
+async def async_setup_entry(hass, config_entry, async_add_entities):
+    """Set up the Terncy light platform."""
+    hub_id = config_entry.data[TERNCY_HUB_ID]
+    hub = hass.data[DOMAIN][hub_id]
 
-async def async_setup_entry(
-    hass, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
-):
-    TerncyEntity.ADD[f"{entry.entry_id}.light"] = async_add_entities
+    def add_entities(devices):
+        async_add_entities(
+            [TerncyLight(hub, device) for device in devices], True
+        )
+
+    hub.add_device_callback("light", add_entities)
 
 
-class TerncyLight(TerncyEntity, LightEntity):
-    """Represents a Terncy light."""
+class TerncyLight(LightEntity):
+    """Representation of a Terncy light."""
 
-    entity_description: TerncyLightDescription
+    def __init__(self, hub, device):
+        """Initialize the light."""
+        self._hub = hub
+        self._device = device
+        self._name = device.get("name", f"Terncy Light {device['id']}")
+        self._id = device["id"]
+        self._available = True
+        self._state = False
+        self._brightness = 0
+        self._hs_color = None
+        self._color_temp = None
+        self._supported_features = 0
+        self._color_mode = ColorMode.UNKNOWN
+        self._supported_color_modes = set()
 
-    _attr_brightness: int | None = None
-    _attr_color_mode: ColorMode | str | None
-    _attr_color_temp_kelvin: int | None = None
-    _attr_min_color_temp_kelvin = 2500
-    _attr_max_color_temp_kelvin = 6500
-    _attr_hs_color: tuple[float, float] | None = None
-    _attr_supported_color_modes: set[ColorMode] | set[str] | None
-    _attr_supported_features: LightEntityFeature
+        self._update_features()
 
-    def __init__(
-        self,
-        gateway,
-        eid: str,
-        description: TerncyLightDescription,
-        init_states: list[AttrValue],
-    ):
-        super().__init__(gateway, eid, description, init_states)
-        self._attr_color_mode = description.color_mode
-        self._attr_supported_color_modes = description.supported_color_modes
-        self._attr_supported_features = description.supported_features
-        if ColorMode.HS in self.supported_color_modes:
-            self._attr_hs_color = (0.0, 0.0)
+    def _update_features(self):
+        """Update supported features and color modes."""
+        features = self._device.get("features", [])
+        self._supported_features = 0
+        self._supported_color_modes = set()
 
-    def update_state(self, attrs):
-        # _LOGGER.debug("%s <= %s", self.eid, attrs)
-        if (on_off := get_attr_value(attrs, "on")) is not None:
-            self._attr_is_on = on_off == 1
-        bri = get_attr_value(attrs, "brightness")
-        if bri:
-            self._attr_brightness = int(bri / 100 * 255)
-        if color_temp_mired := get_attr_value(attrs, "colorTemperature"):
-            self._attr_color_temp_kelvin = color_util.color_temperature_mired_to_kelvin(color_temp_mired)
-            self._attr_color_mode = ColorMode.COLOR_TEMP
-        hue = get_attr_value(attrs, "hue")
-        sat = get_attr_value(attrs, "saturation")
-        if hue is not None:
-            hue = hue / 255 * 360.0
-            self._attr_hs_color = (hue, self._attr_hs_color[1])
-            self._attr_color_mode = ColorMode.HS
-        if sat is not None:
-            sat = sat / 255 * 100
-            self._attr_hs_color = (self._attr_hs_color[0], sat)
-            self._attr_color_mode = ColorMode.HS
-        if self.hass:
-            self.async_write_ha_state()
+        if "brightness" in features:
+            self._supported_features |= SUPPORT_BRIGHTNESS
+            self._supported_color_modes.add(ColorMode.BRIGHTNESS)
+
+        if "color_temp" in features:
+            self._supported_features |= SUPPORT_COLOR_TEMP
+            self._supported_color_modes.add(ColorMode.COLOR_TEMP)
+
+        if "color" in features:
+            self._supported_features |= SUPPORT_COLOR
+            self._supported_color_modes.add(ColorMode.HS)
+
+        # 默认回退逻辑
+        if not self._supported_color_modes:
+            self._supported_color_modes.add(ColorMode.ONOFF)
+
+    @property
+    def unique_id(self):
+        """Return a unique ID."""
+        return self._id
+
+    @property
+    def name(self):
+        """Return the name of the light."""
+        return self._name
+
+    @property
+    def available(self):
+        """Return True if entity is available."""
+        return self._available
+
+    @property
+    def is_on(self):
+        """Return True if light is on."""
+        return self._state
+
+    @property
+    def brightness(self):
+        """Return the brightness of the light."""
+        return self._brightness
+
+    @property
+    def hs_color(self):
+        """Return the hs color of the light."""
+        return self._hs_color
+
+    @property
+    def color_temp(self):
+        """Return the color temperature of the light."""
+        return self._color_temp
+
+    @property
+    def supported_features(self):
+        """Flag supported features."""
+        return self._supported_features
+
+    @property
+    def supported_color_modes(self) -> set[ColorMode] | None:
+        """Flag supported color modes (HA 2026 Strict Check)."""
+        # 核心修复：如果硬件不支持色温，强制只返回亮度或开关模式
+        if not (self._supported_features & SUPPORT_COLOR_TEMP):
+            if self._supported_features & SUPPORT_BRIGHTNESS:
+                return {ColorMode.BRIGHTNESS}
+            return {ColorMode.ONOFF}
+        return self._supported_color_modes
+
+    @property
+    def color_mode(self) -> ColorMode | str | None:
+        """Return the current color mode of the light."""
+        # 核心修复：防止上报硬件不支持的 color_temp 导致 HA 崩溃
+        if not (self._supported_features & SUPPORT_COLOR_TEMP):
+            if self._color_mode == ColorMode.COLOR_TEMP:
+                return ColorMode.BRIGHTNESS
+        return self._color_mode
 
     async def async_turn_on(self, **kwargs):
-        _LOGGER.debug("%s async_turn_on %s", self.eid, kwargs)
-
-        attrs = [{"attr": "on", "value": 1}]
-        self._attr_is_on = True
+        """Turn on the light."""
+        params = {"state": 1}
 
         if ATTR_BRIGHTNESS in kwargs:
-            bri = kwargs.get(ATTR_BRIGHTNESS)
-            terncy_bri = math.ceil(bri / 255 * 100)
-            attrs.append({"attr": "brightness", "value": terncy_bri})
-            self._attr_brightness = bri
+            params["brightness"] = int(kwargs[ATTR_BRIGHTNESS] / 2.55)
 
-        if ATTR_COLOR_TEMP_KELVIN in kwargs:
-            color_temp_kelvin = kwargs.get(ATTR_COLOR_TEMP_KELVIN)
-            if color_temp_kelvin < self._attr_min_color_temp_kelvin:
-                color_temp_kelvin = self._attr_min_color_temp_kelvin
-            elif color_temp_kelvin > self._attr_max_color_temp_kelvin:
-                color_temp_kelvin = self._attr_max_color_temp_kelvin
-            color_temp_mired = color_util.color_temperature_kelvin_to_mired(color_temp_kelvin)
-            attrs.append({"attr": "colorTemperature", "value": color_temp_mired})
-            self._attr_color_temp_kelvin = color_temp_kelvin
-            self._attr_color_mode = ColorMode.COLOR_TEMP
+        if ATTR_COLOR_TEMP in kwargs:
+            # 只有硬件支持时才允许发送色温指令
+            if self._supported_features & SUPPORT_COLOR_TEMP:
+                params["color_temp"] = kwargs[ATTR_COLOR_TEMP]
 
         if ATTR_HS_COLOR in kwargs:
-            hs_color = kwargs.get(ATTR_HS_COLOR)
-            terncy_hue = int(hs_color[0] / 360 * 255)
-            terncy_sat = int(hs_color[1] / 100 * 255)
-            attrs.append({"attr": "hue", "value": terncy_hue})
-            attrs.append({"attr": "saturation", "value": terncy_sat})
-            self._attr_hs_color = hs_color
-            self._attr_color_mode = ColorMode.HS
+            if self._supported_features & SUPPORT_COLOR:
+                params["color"] = {
+                    "h": int(kwargs[ATTR_HS_COLOR][0]),
+                    "s": int(kwargs[ATTR_HS_COLOR][1]),
+                }
 
-        await self.api.set_attributes(self.eid, attrs)
-        self.async_write_ha_state()
+        await self._hub.edit_device(self._id, params)
 
     async def async_turn_off(self, **kwargs):
-        _LOGGER.debug("%s async_turn_off %s", self.eid, kwargs)
-        self._attr_is_on = False
-        await self.api.set_attribute(self.eid, "on", 0)
+        """Turn off the light."""
+        await self._hub.edit_device(self._id, {"state": 0})
+
+    def update_state(self, msg_data):
+        """Update the light state from hub message."""
+        if "state" in msg_data:
+            self._state = msg_data["state"] == 1
+        
+        if "brightness" in msg_data:
+            self._brightness = int(msg_data["brightness"] * 2.55)
+
+        if "color_temp" in msg_data:
+            self._color_temp = msg_data["color_temp"]
+            # 仅当硬件支持时才切换到色温模式，否则降级为亮度模式
+            if self._supported_features & SUPPORT_COLOR_TEMP:
+                self._color_mode = ColorMode.COLOR_TEMP
+            else:
+                self._color_mode = ColorMode.BRIGHTNESS
+
+        if "color" in msg_data:
+            color = msg_data["color"]
+            self._hs_color = (color.get("h", 0), color.get("s", 0))
+            self._color_mode = ColorMode.HS
+
+        # 兜底逻辑：确保 color_mode 永远有效
+        if self._color_mode == ColorMode.UNKNOWN or self._color_mode is None:
+            if self._supported_features & SUPPORT_COLOR:
+                self._color_mode = ColorMode.HS
+            elif self._supported_features & SUPPORT_COLOR_TEMP:
+                self._color_mode = ColorMode.COLOR_TEMP
+            elif self._supported_features & SUPPORT_BRIGHTNESS:
+                self._color_mode = ColorMode.BRIGHTNESS
+            else:
+                self._color_mode = ColorMode.ONOFF
+
         self.async_write_ha_state()
-
-
-TerncyEntity.NEW["light"] = TerncyLight
